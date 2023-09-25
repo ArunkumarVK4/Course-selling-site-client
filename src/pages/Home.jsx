b7079443ae03665118dd1a808a18228a11532146
ArunkumarVK <vkarunkumar446@gmail.com>
2023-09-25 15:08:52 +0530

    addde Sample Login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,10 +13,30 @@ export default function Home() {
             "Discover a world of knowledge with our diverse range of online courses, designed to
             empower learners of all levels and interests."
           </p>
-          <p className='m-8 border  border-indigo-900 p-8 bg-cyan-700 rounded-xl'>From coding to cooking, our platform offers expert-led courses that cater to your passions and aspirations, all in one convenient place.</p>
-          <p className='m-8 border  border-indigo-900 p-8 bg-cyan-700 rounded-xl'>Join a community of lifelong learners, where you can explore, grow, and achieve your goals through engaging and interactive online courses.</p>
+          <p className='m-8 border  border-indigo-900 p-8 bg-cyan-700 rounded-xl'>
+            From coding to cooking, our platform offers expert-led courses that cater to your
+            passions and aspirations, all in one convenient place.
+          </p>
+          <p className='m-8 border  border-indigo-900 p-8 bg-cyan-700 rounded-xl'>
+            Join a community of lifelong learners, where you can explore, grow, and achieve your
+            goals through engaging and interactive online courses.
+          </p>
         </div>
       </div>
+      <table className='mx-auto rounded border-gray-500'>
+        <tr className='border-blue-900 bg-slate-800'>
+          <th className='text-white text-xl '>User Login </th>
+          <th className='text-white text-xl'>Admin Login</th>
+        </tr>
+        <tr>
+          <td className='text-lg py-4'>User Name: arun@gmail.com</td>
+          <td className='text-lg px-14'>User Name: arunadmin@gmail.com</td>
+        </tr>
+        <tr>
+          <td className='text-lg'>Password: 1234</td>
+          <td className='text-lg'>User Password: 1234</td>
+        </tr>
+      </table>
     </>
   );
 }
